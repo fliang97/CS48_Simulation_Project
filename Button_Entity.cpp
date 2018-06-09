417ae284a538067a5cdcad8976b08cee4fbae8e2
Erez Hajaj <erezhajaj@gmail.com>
2018-06-08 17:11:10 -0700

    fixed button rendering

--- Button_Entity.cpp
+++ Button_Entity.cpp
@@ -18,8 +18,24 @@ Button_Entity::Button_Entity(int x, int y, int w, int h, SDL_Renderer* r, int id
 	//TO DO: Set Images
 	//unclicked_img = IMG_LoadTexture(renderer, "grass3.png");
 	//clicked_img = IMG_LoadTexture(renderer, "grass3.png");
-	current_img = unclicked_img;
 	//TO DO: Reference to scale
+	Cow::setTextureImg();
+	Cow::setTextureImgSick();
+	Wolf::setTextureImg();
+	Wolf::setTextureImgSick();
+	Grass::setTextureImg();
+	//cout << "SET IMAGE" << endl;
+	if(EntityID == 3) {
+		unclicked_img = Cow::static_img;
+		clicked_img = Cow::static_img_sick;
+	} else if (EntityID == 4) {
+		unclicked_img = Grass::static_img;
+		clicked_img = Grass::static_img;
+	} else if (EntityID == 5) {
+		unclicked_img = Wolf::static_img;
+		clicked_img = Wolf::static_img_sick;
+	}
+		current_img = unclicked_img;
 }
 
 void Button_Entity::clicked() {
@@ -38,16 +54,6 @@ void Button_Entity::render() {
 	rect.y = ypos;
 	rect.w = width;
 	rect.h = height;
-	if(EntityID == 3) {
-		unclicked_img = Cow::static_img;
-		clicked_img = Cow::static_img_sick;
-	} else if (EntityID == 4) {
-		unclicked_img = Grass::static_img;
-		clicked_img = Grass::static_img;
-	} else if (EntityID == 5) {
-		unclicked_img = Wolf::static_img;
-		clicked_img = Wolf::static_img_sick;
-	}
 
 	SDL_RenderCopy(renderer, current_img, NULL, &rect);
 }
